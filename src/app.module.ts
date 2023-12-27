e6b499969429b74ff18d0c0acf273a8c185ef658
qadoos <qadoos@172-0-0-77.lightspeed.brhmal.sbcglobal.net>
2023-12-27 14:55:19 +0500

    auth login setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmailsModule } from './emails/emails.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [EmailsModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
