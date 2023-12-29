1de5d0f7bcd5c5363e64ffe5455c3ba3a65f0596
qadoos <qadoos@172-0-0-77.lightspeed.brhmal.sbcglobal.net>
2023-12-29 17:17:10 +0500

    remove emails

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EmailsModule } from './emails/emails.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 
 @Module({
-  imports: [EmailsModule, AuthModule, UsersModule,
+  imports: [AuthModule, UsersModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: '127.0.0.1',
