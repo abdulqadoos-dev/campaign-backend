3dfdec71bf03c6d84b964238d8a9df22cbf84360
qadoos <qadoos@172-0-0-162.lightspeed.brhmal.sbcglobal.net>
2024-01-09 18:30:38 +0500

    connect to frontend

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -11,12 +11,14 @@ export class AuthService {
   constructor(private jwtService: JwtService, @InjectRepository(User) private userRepository: Repository<User>) { }
 
   async autenticate(username: string, password: string) {
-
-    const user = await this.userRepository.findOneBy({ username });
+    const user = await this.userRepository.findOne({ where: { username } });
     if (!user) throw new UnauthorizedException();
+
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) throw new UnauthorizedException();
-    return { access_token: await this.jwtService.signAsync({ sub: user.id, username: user.username }) };
+    
+    let { name, email, role, } = user;
+    return { name, email, role, accessToken: await this.jwtService.signAsync({ sub: user.id, username: user.username }) };
   }
 
 
