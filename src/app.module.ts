8c626e0ed3c7c45b5207fa788c5d540339dfb13e
abdul qadoos <abdulqadoos.work@gmail.com>
2024-01-15 18:37:04 +0500

    credential form db

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,19 +18,19 @@ import { CampaignsModule } from './campaigns/campaigns.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({isGlobal: true}),
+    ConfigModule.forRoot({ isGlobal: true }),
 
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '127.0.0.1',
+      host: process.env.DB_HOST,
       port: 3306,
-      username: 'root',
-      password: '',
-      database: 'campaigns',
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [User, Campaign, Lead],
-      synchronize: true,
+      synchronize: process.env.SYNC === 'true',
     }),
-    
+
     AuthModule, UsersModule, CampaignsModule, LeadsModule,
   ],
   controllers: [AppController],
@@ -38,6 +38,6 @@ import { CampaignsModule } from './campaigns/campaigns.module';
 })
 
 
-export class AppModule { 
-  constructor(private dataSource: DataSource) {}
+export class AppModule {
+  constructor(private dataSource: DataSource) { }
 }
